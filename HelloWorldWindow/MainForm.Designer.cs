90f220aeda505d33233321f62c8e489bd0ed7edc
Masha <Masha@DESKTOP-IOHF3KI>
2018-11-17 22:12:07 +0200

    Create HelloWorldWindow

---
+++ HelloWorldWindow/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace HelloWorldWindow
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helloButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(355, 189);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(117, 61);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "HELLO!";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.helloButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(284, 48);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(259, 26);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(623, 94);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.helloButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ HelloWorldWindow/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace HelloWorldWindow
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Text = string.Empty;
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            var actualName = GetActualName(name);
+
+            MessageBox.Show($"Hello, {actualName}!");
+        }
+
+        private string GetActualName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return "OOOO";
+
+            var actualName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name.ToLower());
+
+            return actualName;
+        }
+
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var text = nameTextBox.Text;
+            this.Text = text;
+        }
+    }
+}
